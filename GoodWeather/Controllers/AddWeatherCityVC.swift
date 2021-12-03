f7c25da9a06f961ee8d7860fc0b58d95787bfe32
abdelrahmansalahkhamis <abdulrahmansalah86@gmail.com>
2021-12-03 19:41:39 +0200

    ddddddd

---
+++ GoodWeather/Controllers/AddWeatherCityVC.swift
@@ -0,0 +1,26 @@
+//
+//  AddWeatherCityVC.swift
+//  GoodWeather
+//
+//  Created by abdrahman on 01/12/2021.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityVC: UIViewController {
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func saveCityButtonPressed(){
+        
+    }
+    
+    @IBAction func close(){
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ GoodWeather/CustomCells/WeatherCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by abdrahman on 01/12/2021.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    @IBOutlet weak var cityNameLbl: UILabel!
+    
+    @IBOutlet weak var weatherLbl: UILabel!
+    
+}
