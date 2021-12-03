b5b9dc1821e8acf6d07eab2f5b42e1ff347e1fdd
abdelrahmansalahkhamis <abdulrahmansalah86@gmail.com>
2021-12-03 20:14:16 +0200

    kkkkkkkk

--- GoodWeather/Controllers/AddWeatherCityVC.swift
+++ GoodWeather/Controllers/AddWeatherCityVC.swift
@@ -14,6 +14,7 @@ class AddWeatherCityVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("---------")
     }
     
     @IBAction func saveCityButtonPressed(){
